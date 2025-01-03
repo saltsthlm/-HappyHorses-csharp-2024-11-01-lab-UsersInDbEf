b6a579e64ca9f51a4000725dd63dc020583702a8
Junita Berglin <junita.berglin@gmail.com>
2025-01-03 16:40:26 +0100

    filter
    Co-authored-by: Julia Rutfjall <julia.rutfjall@gmail.com>
    Co-authored-by: Konrad Drozdyk <konrad.drozdyk@gmail.com>
    Co-authored-by: Alex Saxena <saxenaalex@gmail.com>

--- UserAddressDbEf/Program.cs
+++ UserAddressDbEf/Program.cs
@@ -1,29 +1,42 @@
 ﻿using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using UserAddressDbEf;
 using UserAddressDbEf.Models;
 using UserAddressDbEf.Services;
 
-class Program
+
+using var db = new AppDbContext();
+
+Console.WriteLine("Application started.");
+
+DatabaseSeeder.Seed(db);
+
+if (args.Length < 1)
 {
-    static void Main(string[] args)
+    Console.WriteLine("All Users!");
+    var queryAllUsers = db.Users.OrderBy(user => user.FirstName);
+
+    foreach (var user in queryAllUsers)
     {
-
-        using var db = new AppDbContext();
-
-        Console.WriteLine("Application started.");
-
-        DatabaseSeeder.Seed(db);
-
-        if (args.Length < 1)
-        {
-            Console.WriteLine("All Users!");
-            var queryAllUsers = db.Users.OrderBy(user => user.FirstName);
-
-            foreach (var user in queryAllUsers)
-            {
-                Console.WriteLine(user.FirstName + " " + user.LastName);
-            }
-        }
+        Console.WriteLine(user.FirstName + " " + user.LastName);
     }
+}
+var inputValue = args[0];
 
+
+static void filterByName(string inputValue)
+{
+    using var db = new AppDbContext();
+
+    var filteredArray = db.Users
+                    .Where(user => user.FirstName
+                    .StartsWith(inputValue))
+                    .Select(user => new { user.FirstName, user.LastName })
+                    .ToList();
+
+    foreach (var user in filteredArray)
+    {
+        Console.WriteLine($"{user.FirstName}, {user.LastName}");
+    }
 }
