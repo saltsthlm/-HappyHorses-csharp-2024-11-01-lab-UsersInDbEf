91358d7dd66bb313465ce40229f1596d6f53bd0c
Alex <saxenaalex@gmail.com>
2025-01-03 14:12:44 +0100

    Adds request to db for users
    Co-authored-by: Julia Rutfjall <julia.rutfjall@gmail.com>
    Co-authored-by: Junita Berglin <junita.berglin@gmail.com>
    Co-authored-by: Konrad Drozdyk <konrad.drozdyk@gmail.com>

--- UserAddressDbEf/Program.cs
+++ UserAddressDbEf/Program.cs
@@ -1,4 +1,5 @@
-﻿using UserAddressDbEf;
+﻿using System.Data.Common;
+using UserAddressDbEf;
 using UserAddressDbEf.Models;
 using UserAddressDbEf.Services;
 
@@ -6,12 +7,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        using var context = new AppDbContext();
+        using var db = new AppDbContext();
 
         // Seed the database
-        DatabaseSeeder.Seed(context);
+        DatabaseSeeder.Seed(db);
 
         Console.WriteLine("Application started.");
-        Console.WriteLine(args[0]);
+        // Console.WriteLine(args[0]);
+
+
+        // Console.WriteLine("Please search by ID o Name");
+        // var searchInput = Console.ReadLine();
+
+        // Console.WriteLine("You searched for " + searchInput);
+
+        Console.WriteLine("All Users!");
+        var queryAllUsers = db.Users.OrderBy(user => user.FirstName);
+
+        foreach (var user in queryAllUsers)
+        {
+            Console.WriteLine(user.FirstName);
+        }
     }
 }
